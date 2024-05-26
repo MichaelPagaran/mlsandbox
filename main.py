3419d25d070915560bdc63564f4b308b3517b66d
Michael Pagaran <mmpagaran64@gmail.com>
2024-05-26 22:12:11 +0800

    add student dataset

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import tensorflow
-import keras
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
 
 print('Hello World!')
